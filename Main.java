1d475ff60a6f0595ba875f2d957a83340e6a3596
Nam Nguyen <namnguyen@cpp.edu>
2021-11-26 21:06:57 -0800

    Add adjacencyMatrix, pool functions and theirs test codes

--- Main.java
+++ Main.java
@@ -1,3 +1,70 @@
+import java.util.Random;
+
 public class Main {
+    public static void main(String[] args) {
+        boolean dense = true;
+        boolean sparse = false;
+
+        int [][] matrix = adjacencyMatrix(4, dense);
+
+        display(matrix);
+    }
+
+    private static int[][] adjacencyMatrix(int size, boolean typeOfGraph) {
+
+        int number = 0;
+
+        int[][] matrix = new int[size][size];
+
+        Random rand = new Random();
+        
+        for(int row = 0; row < size; row++) {
+            for(int col = 0; col < size; col++) {
+                if(row == col) {
+                    matrix[row][col] = 0;
+                }
+                else {
+                    number = pool(rand.nextInt(10) + 1, typeOfGraph);
+                    matrix[row][col] = number ;
+                    matrix[col][row] = number ;
+                }
+            }
+        }
+
+        return matrix;
+    }
+
+    private static int pool(int number, boolean typeOfGraph) {
+
+        Random rand = new Random();
+
+        //Dense graph (7:3)
+        if(typeOfGraph == true ) {
+            if(number <= 7) {
+                return rand.nextInt(100);
+            }
+            else {
+                return -1;
+            }
+        }
+        //Sparse graph (3:7)
+        else {
+            if(number >= 7) {
+                return rand.nextInt(100);
+            }
+            else {
+                return -1;
+            }
+        }
+    }
+
+    private static void display(int[][] matrix) {
+        for(int row = 0; row < matrix.length; row++) {
+            for(int col = 0; col < matrix[0].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
     
 }
